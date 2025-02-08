0f94c12ef4e6c93aea3edfdf56efb7691cfaf9b4
uzairkhans24 <166864757+uzairkhans24@users.noreply.github.com>
2025-02-07 16:07:12 -0800

    Update product.ts

--- src/sanity/schemaTypes/product.ts
+++ src/sanity/schemaTypes/product.ts
@@ -1,4 +1,4 @@
-export default {
+const productSchema ={
     name: 'product',
     title: 'Product',
     type: 'document',
@@ -61,3 +61,4 @@ export default {
       },
     ],
   };
+  export default productSchema;
